25a48c3b53cfd1b066442456af9eafb79ec85831
princ0301 <prince07190@gmail.com>
2025-02-25 10:55:25 +0530

    agent initialized

---
+++ agent.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    WorkerOptions,
+    cli,
+    llm
+)
+
+from livekit.agents.multimodal import MultimodalAgent
+from livekit.plugins import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+async def entrypoint(ctx: JobContext):
+    await ctx.connect(auto_subscribe=AutoSubscribe.SUBSCRIBE_ALL)
+    await ctx.wait_for_participant()
+
+    model = openai.realtime.RealtimeModel(
+        instructions="",
+        voice="shimmer",
+        temperature=0.8,
+        modalities=["audio", "text"]
+    )
+    
